663130cae5ad0fe3a2b58eb2500f88fbdfe6af5e
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-21 10:54:57 +0900

    Initial Commit

---
+++ AomgSocialNetwork/AomgSocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AomgSocialNetworkApp.swift
+//  AomgSocialNetwork
+//
+//  Created by David Goggins on 2023/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AomgSocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
